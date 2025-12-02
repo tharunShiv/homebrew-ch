58312d25b7f1a85beea9f41f5676e71ad2adfb63
tharunShiv <taunchan@MacBook.local>
2025-12-02 12:36:23 +0530

    v1.4.1

--- Casks/command-helper.rb
+++ Casks/command-helper.rb
@@ -1,7 +1,7 @@
 cask "command-helper" do
-  version "1.4.0"
+  version "1.4.1"
   # Replace the line below with the output of the shasum command (see step 4)
-  sha256 "d3bb20566b427dc1755651e000980f3bda37c5c152f368c60d1d11159a0b25d4"
+  sha256 "e6cc600bd899d7562f074c3121be4946125076a37ee3550eb1bf3c525107d265"
 
   url "https://github.com/tharunShiv/command-helper/releases/download/v#{version}/Command.Helper-#{version}-arm64.dmg"
 
